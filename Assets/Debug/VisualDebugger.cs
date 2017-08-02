c5a737b232873bfb295be168c957dfedffe7c8a0
Justin Patterson <jpatt94@fullsail.edu>
2017-08-02 05:53:40 -0500

    Adding assets

---
+++ Assets/Debug/VisualDebugger.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class VisualDebugger : MonoBehaviour
+{
+	private Canvas canvas;
+    private List<string> entries;
+    private Text variableTracking;
+
+    static VisualDebugger instance = null;
+
+    void Start()
+    {
+		canvas = GetComponent<Canvas>();
+		canvas.enabled = false;
+        entries = new List<string>();
+		variableTracking = Utility.FindChild(gameObject, "VariableTracking").GetComponent<Text>();
+        instance = this;
+    }
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F3))
+		{
+			canvas.enabled = !canvas.enabled;
+		}
+	}
+
+    void LateUpdate()
+    {
+        if (entries.Count > 0)
+        {
+			variableTracking.text = "";
+
+            foreach (string s in entries)
+            {
+				variableTracking.text += s;
+				variableTracking.text += "\n";
+            }
+        }
+
+        entries.Clear();
+    }
+
+	/**********************************************************/
+	// Interface
+
+	public static void TrackVariable(string variableName, object variable)
+    {
+        if (instance != null)
+        {
+            for (int i = 0; i < instance.entries.Count; i++)
+            {
+                string s = instance.entries[i];
+                string varName = "";
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[j] == ':')
+                    {
+                        break;
+                    }
+
+                    varName += s[j];
+                }
+
+                if (varName == variableName)
+                {
+                    instance.entries[i] = variableName + ": " + variable.ToString();
+                    return;
+                }
+            }
+
+            instance.entries.Add(variableName + ": " + variable.ToString());
+        }
+    }
+
+	public static bool Enabled
+	{
+		get
+		{
+			return instance.canvas.enabled;
+		}
+		set
+		{
+			instance.canvas.enabled = value;
+		}
+	}
+}
